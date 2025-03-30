bd4955daef623ec350aaba08af461c5aefe172b2
Isikcan Yilmaz <can158@gmail.com>
2025-03-30 10:26:17 +0200

    makescript made so that it programs multiple ports

--- remote/my_stuff/00_basic_ccn/makescript.sh
+++ remote/my_stuff/00_basic_ccn/makescript.sh
@@ -23,29 +23,32 @@ fi
 $RIOTBASE/dist/tools/uf2/uf2conv.py -f 0xADA52840 bin/"$BOARD"/"$APPLICATION".hex --base 0x1000 -o bin/"$BOARD"/"$APPLICATION".uf2 -c
 
 if [ "$1" ]; then
-  PORT="$1"
-  echo "$PORT AS DEVICE TO PROGRAM"
+  PORT=( "$@" )
+  echo "${PORT[@]} AS DEVICE(S) TO PROGRAM"
 fi
 
 # If port info supplied
 if [ "$PORT" ]; then
-  # copy our uf2 file into the volume
-  uname -a | grep -i linux
-  if [ $? == 0 ]; then # If linux
-    # set our device to boot mode
-    stty -F $PORT raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
-    echo "WAITING FOR VOLUME TO COME UP"
-    sleep 3
-    cp bin/"$BOARD"/"$APPLICATION".uf2 /media/"$(whoami)"/XIAO-SENSE/
-  else # If macos
-    # set our device to boot mode
-    stty -f $PORT raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
-    echo "WAITING FOR VOLUME TO COME UP"
-    sleep 3
-    cp bin/"$BOARD"/"$APPLICATION".uf2 /Volumes/XIAO-SENSE/
-  fi
+  for p in ${PORT[@]}; do
+    echo "$p"
+    # copy our uf2 file into the volume
+    uname -a | grep -i linux
+    if [ $? == 0 ]; then # If linux
+      # set our device to boot mode
+      stty -F "$p" raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
+      echo "WAITING FOR VOLUME TO COME UP"
+      sleep 3
+      cp bin/"$BOARD"/"$APPLICATION".uf2 /media/"$(whoami)"/XIAO-SENSE/
+    else # If macos
+      # set our device to boot mode
+      stty -f "$p" raw ispeed 1200 ospeed 1200 cs8 -cstopb ignpar eol 255 eof 255
+      echo "WAITING FOR VOLUME TO COME UP"
+      sleep 3
+      cp bin/"$BOARD"/"$APPLICATION".uf2 /Volumes/XIAO-SENSE/
+    fi
 
-  echo "PROGRAMMED $PORT"
+    echo -e "${GREEN}PROGRAMMED $p${NC}"
+  done
 fi
 
 echo "DONE"

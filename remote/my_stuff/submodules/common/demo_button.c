049c8bc28c7a3b8a3750ea048baa6e2c3cd2070c
jon <isikcan.yilmaz@tu-dresden.de>
2025-04-02 16:26:51 +0200

    updates

--- remote/my_stuff/submodules/common/demo_button.c
+++ remote/my_stuff/submodules/common/demo_button.c
@@ -8,21 +8,46 @@
 #include <stdio.h>
 
 gpio_t buttonGpios[NUM_BUTTONS] = {
-  [BUTTON_RED] = GPIO_PIN(1,15),
-  [BUTTON_GREEN] = GPIO_PIN(1,14),
+  [BUTTON_RED] = GPIO_PIN(1,12),
+  [BUTTON_GREEN] = GPIO_PIN(1,13),
   [BUTTON_BLUE] = GPIO_PIN(1,14),
-  [BUTTON_SHIFT] = GPIO_PIN(1,12)
+  [BUTTON_SHIFT] = GPIO_PIN(1,15)
 };
 
-char Button_ThreadStack[THREAD_STACKSIZE_DEFAULT];
+char buttonThreadStack[THREAD_STACKSIZE_DEFAULT];
 
 kernel_pid_t buttonThreadId; // TODO decide if you want this to be here
 
+bool buttonStates[NUM_BUTTONS];
+bool buttonStates[NUM_BUTTONS];
+
+bool buttonTriggered = false;
+
 /////////////////////////////////////////
 
-static void ButtonIrq(void *arg)
+static void Button_Read(void)
 {
-  printf("BUTTON IRQ\n");
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
+    printf("%d ", gpio_read(buttonGpios[i]));
+  }
+  printf("\n");
+}
+
+static void Button_IrqHandler(void *arg)
+{
+  buttonTriggered = true;
+}
+
+/////////////////////////////////////////
+
+static void *Button_ThreadHandler(void *arg)
+{
+  if (!buttonTriggered)
+  {
+    return;
+  }
+  Button_Read();
 }
 
 /////////////////////////////////////////
@@ -31,7 +56,8 @@ void Button_Init(void)
 {
   for (int i = 0; i < NUM_BUTTONS; i++)
   {
-    gpio_init_int(buttonGpios[i], GPIO_IN_PD, GPIO_RISING, ButtonIrq, NULL);
+    buttonStates[i] = false;
+    gpio_init_int(buttonGpios[i], GPIO_IN_PD, GPIO_BOTH, Button_IrqHandler, NULL);
     gpio_irq_enable(buttonGpios[i]);
   }
 }

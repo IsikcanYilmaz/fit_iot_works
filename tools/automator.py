0f7a4fada7b7ca7d4faab693175acc210b0787ff
Isikcan Yilmaz <can158@gmail.com>
2025-07-13 19:32:49 +0200

    fitiot sync

--- tools/automator.py
+++ tools/automator.py
@@ -68,8 +68,13 @@ def resetNetstats(dev):
             
 def getAddresses(dev):
     global comm
-    outStrRaw = comm.sendSerialCommand(dev, "ifconfig")
-    parseIfconfig(dev, outStrRaw)
+    count = 0
+    while (count < 3 and "globalAddr" not in dev.keys()):
+        outStrRaw = comm.sendSerialCommand(dev, "ifconfig", cooldownS=0.1)
+        parseIfconfig(dev, outStrRaw)
+        if ("globalAddr" in dev.keys()):
+            break
+        count += 1
 
 def setGlobalAddress(dev):
     global comm

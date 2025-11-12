016effe18456ad0669f1d75bc4d376f08b77de7c
jon <isikcan.yilmaz@tu-dresden.de>
2025-11-12 12:53:11 +0100

    revert automator changes. need to do em in post processing

--- tools/automator.py
+++ tools/automator.py
@@ -224,12 +224,12 @@ def averageRoundsJsons(j):
     avgSumCacheHits = sum([j[i]["results"]["sumCacheHits"] for i in range(0, len(j))])/len(j)
     avgl2sentPackets = sum([j[i]["results"]["l2sumSentPackets"] for i in range(0, len(j))])/len(j)
     avgl2receivedPackets = sum([j[i]["results"]["l2sumReceivedPackets"] for i in range(0, len(j))])/len(j)
-    avgIpv6sentPackets = sum([j[i]["results"]["ipv6sumSentPackets"] for i in range(0, len(j))])/len(j)
-    avgIpv6receivedPackets = sum([j[i]["results"]["ipv6sumReceivedPackets"] for i in range(0, len(j))])/len(j)
+    # avgIpv6sentPackets = sum([j[i]["results"]["ipv6sumSentPackets"] for i in range(0, len(j))])/len(j)
+    # avgIpv6receivedPackets = sum([j[i]["results"]["ipv6sumReceivedPackets"] for i in range(0, len(j))])/len(j)
     avgNumSentPkts = sum([j[i]["results"]["numSentPkts"] for i in range(0, len(j))])/len(j)
     avgNumForwards = sum([j[i]["results"]["numForwards"] for i in range(0, len(j))])/len(j)
     numDatapoints = len(j)
-    return {"avgLostPackets":avgNumLostPkts, "avgLossPercent":avgLossPercent, "avgSendRate":avgSendRate, "avgReceiveRate":avgReceiveRate, "avgTimeDiffSecs":avgTimeDiffSecs, "avgSumCacheHits":avgSumCacheHits, "numDatapoints":numDatapoints, "avgNumSentPkts":avgNumSentPkts, "avgNumForwards":avgNumForwards, "avgL2sentPackets": avgl2sentPackets, "avgL2receivedPackets":avgl2receivedPackets, "avgIpv6sentPackets": avgIpv6sentPackets, "avgIpv6receivedPackets":avgIpv6receivedPackets}
+    return {"avgLostPackets":avgNumLostPkts, "avgLossPercent":avgLossPercent, "avgSendRate":avgSendRate, "avgReceiveRate":avgReceiveRate, "avgTimeDiffSecs":avgTimeDiffSecs, "avgSumCacheHits":avgSumCacheHits, "numDatapoints":numDatapoints, "avgNumSentPkts":avgNumSentPkts, "avgNumForwards":avgNumForwards, "avgL2sentPackets": avgl2sentPackets, "avgL2receivedPackets":avgl2receivedPackets}
 
 async def resetAllDevicesNetstats():
     global devices, ifaceId
@@ -600,9 +600,6 @@ async def cachingExperiment(delayus=10000, payloadsizebytes=32, transfersizebyte
 
     print(f"Results written to {resultsDir}")
     print("----------------")
-
-def bulkCachingExperiments(resultsDir):
-    pass
 
 def tester(dev):
     getL2Stats(dev)
